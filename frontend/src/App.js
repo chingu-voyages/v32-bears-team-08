41dfc25ad0de7cd6e80772bccb1ce5843a30b6da
a-w-m <mamrey@gmail.com>
2021-08-08 13:00:48 -0400

    Create Login/Register Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useReducer, useState, useEffect } from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import logo from "./logo.svg";
+import "./App.css";
+
+
+
+const initialState = {
+	user: null,
+	auth: false,
+};
+
+export const userContext = createContext(null);
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "SET_USER":
+			return {
+				...state,
+				user: action.payload
+			};
+		case "SET_AUTH":
+			return{
+				...state,
+					token: action.payload
+				
+			}
+		default:
+			return state;
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const [register, setRegister] = useState(false);
+
+
+
+	return (
+		<userContext.Provider value={{ appState: state, appDispatch: dispatch }}>
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+				<>{!register ? <Login /> : <Register />}</>
+				<button
+					onClick={(e) => {
+						setRegister((prev) => !prev);
+					}}
+				>{`${
+					register ? "Click here to login" : "Not registered? Click here"
+				}`}</button>
+			</div>
+		</userContext.Provider>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/Register/index.js
@@ -0,0 +1,140 @@
+import React, { useReducer } from "react";
+import "./style.css";
+import authServices from "../../services/auth";
+const initialState = {
+	name: "",
+	email: "",
+	password: "",
+	confirmPassword: "",
+	error: "",
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "SET_NAME":
+			return {
+				...state,
+				name: action.payload,
+			};
+		case "SET_EMAIL":
+			return {
+				...state,
+				email: action.payload,
+			};
+		case "SET_PASSWORD":
+			return {
+				...state,
+				password: action.payload,
+			};
+		case "SET_CONFIRM_PASSWORD":
+			return {
+				...state,
+				confirmPassword: action.payload,
+			};
+		case "SET_ERROR":
+			return {
+				...state,
+				error: action.payload,
+			};
+	}
+}
+
+function Register() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const data =  {
+			name: state.name,
+			email: state.email,
+			password: state.password,
+			confirmPassword: state.confirmPassword
+		}
+
+		try{
+
+			let res =  await authServices.postRegister(data)
+			console.log(res)
+
+		} catch (error){
+			dispatch({
+				type: "SET_ERROR",
+				payload: error.message,
+			})
+		}
+
+		// if (res.status!='ok'){
+		// 	dispatch({
+		// 		type: "SET_ERROR",
+		// 		payload: res.error,
+		// 	})
+		// }
+		
+		
+	}
+	return (
+		<>
+			<form
+				onSubmit={(e) => {
+					handleSubmit(e);
+				}}
+			>
+				<div>Register</div>
+				<label htmlFor="name">
+				<input
+						type="name"
+						id="name"
+						name="name"
+						placeholder="Name"
+						onChange={(e) => {
+							dispatch({ type: "SET_NAME", payload: e.target.value });
+						}}
+					></input>
+					</label>
+					<br/>
+					<label htmlFor="email">
+					<input
+						type="email"
+						id="email"
+						name="email"
+						placeholder="Email"
+						onChange={(e) => {
+							dispatch({ type: "SET_EMAIL", payload: e.target.value });
+						}}
+					></input>
+				</label>
+				<br />
+				<label htmlFor="password">
+					<input
+						type="password"
+						id="password"
+						name="password"
+						placeholder="Password"
+						onChange={(e) => {
+							dispatch({ type: "SET_PASSWORD", payload: e.target.value });
+						}}
+					></input>
+				</label>
+				<label htmlFor="confirmPassword">
+					<input
+						type="password"
+						id="confirmPassword"
+						name="confirmPassword"
+						placeholder="Confirm Password"
+						onChange={(e) => {
+							dispatch({
+								type: "SET_CONFIRM_PASSWORD",
+								payload: e.target.value,
+							});
+						}}
+					></input>
+				</label>
+				<br />
+				<span>{state.error}</span>
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	);
+}
+
+export default Register;
