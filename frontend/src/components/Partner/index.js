14625a1d46caa82d2ca044e27baf956cafd9641d
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-08-18 11:41:17 -0700

    created Partner component

---
+++ frontend/src/components/Partner/index.js
@@ -0,0 +1,13 @@
+import Email from "../Email";
+import styles from "./style.module.css";
+
+function Partner() {
+  return (
+    <div className={styles["partner"]}>
+      <h1>partner profile goes here</h1>
+      <Email></Email>
+    </div>
+  );
+}
+
+export default Partner;
