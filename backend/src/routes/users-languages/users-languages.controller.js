5f8c69ddcd718cf7e7c2a6c8ffd13b9024dbcf5f
a-w-m <mamrey@gmail.com>
2021-08-23 11:44:08 -0400

    Get user languages

--- backend/src/routes/users-languages/users-languages.controller.js
+++ backend/src/routes/users-languages/users-languages.controller.js
@@ -1,46 +1,31 @@
 const service = require("./users-languages.service");
-const languageService = require("../languages/languages.service")
 const asyncErrorBoundary = require("../../errors/asyncErrorBoundary");
 
 async function list(req, res, next) {
-    const response = await service.list();
-    return res.json({
-        data: response,
-    })
+	const response = await service.list();
+	return res.json({
+		data: response,
+	});
 }
 
 async function find(req, res, next) {
-    const response = await service.find(req.params.user-language_id);
-    if (response[0]) {
-        return res.json({ 
-            data: response[0],
-        });
-    }
-    next({
-        status: 404,
-        message: `users-language ${req.params.user-language_id} not found`,
-    })
+	const response = await service.find(req.params.user - language_id);
+	if (response[0]) {
+		return res.json({
+			data: response[0],
+		});
+	}
+	next({
+		status: 404,
+		message: `users-language ${req.params.user - language_id} not found`,
+	});
 }
 
 async function findUserLanguages(req, res, next) {
 	const response = await service.findByUser(req.params.user_id);
 
-	if (response[0]) {
-		const languages = await response.map(async (ele) => {
-			const language = await languageService.find(ele.language);
-			return language[0];
-		})
-
-		const data = await Promise.all(languages);
-
-		return res.json({
-			data: data,
-		});
-	}
-
-	next({
-		status: 404,
-		message: `user ${req.params.user_id} not found`,
+	return res.json({
+		data: response,
 	});
 }
 
@@ -52,70 +37,60 @@ async function create(req, res, next) {
 }
 
 async function remove(req, res, next) {
-    const response = await service.remove(req.params.user-language_id);
-    return res.json({
-        data: response[0],
-    })
+	const response = await service.remove(req.params.user - language_id);
+	return res.json({
+		data: response[0],
+	});
 }
 
 function hasData(req, res, next) {
-    if (req.body.data) {
-        return next();
-    }
-    next({
-        status: 400,
-        message: "request body must have data property",
-    })
+	if (req.body.data) {
+		return next();
+	}
+	next({
+		status: 400,
+		message: "request body must have data property",
+	});
 }
 
 function hasUser(req, res, next) {
-    if (req.body.data.user) {
-        return next();
-    }
-    next({
-        status: 400,
-        message: "request body data must have user property",
-    })
+	if (req.body.data.user) {
+		return next();
+	}
+	next({
+		status: 400,
+		message: "request body data must have user property",
+	});
 }
 
 function hasLanguage(req, res, next) {
-    if (req.body.data.language) {
-        return next();
-    }
-    next({
-        status: 400,
-        message: "request body data must have language property",
-    })
+	if (req.body.data.language) {
+		return next();
+	}
+	next({
+		status: 400,
+		message: "request body data must have language property",
+	});
 }
 
 function hasFluency(req, res, next) {
-    if (req.body.data.fluency) {
-        return next();
-    }
-    next({
-        status: 400,
-        message: "request body data must have fluency property",
-    })
+	if (req.body.data.fluency) {
+		return next();
+	}
+	next({
+		status: 400,
+		message: "request body data must have fluency property",
+	});
 }
 
 module.exports = {
-    list: [
-        asyncErrorBoundary(list),
-    ],
-    find: [
-        asyncErrorBoundary(find),
-    ],
-    findUserLanguages:[
-        asyncErrorBoundary(findUserLanguages)
-    ],
-    create: [
-        hasData,
-        hasUser,
-        hasLanguage,
-        hasFluency,
-        asyncErrorBoundary(create),
-    ],
-    remove: [
-        asyncErrorBoundary(remove),
-    ],
-}
+	list: [asyncErrorBoundary(list)],
+	find: [asyncErrorBoundary(find)],
+	findUserLanguages: [asyncErrorBoundary(findUserLanguages)],
+	create: [
+		hasData,
+		hasLanguage,
+		asyncErrorBoundary(create),
+	],
+	remove: [asyncErrorBoundary(remove)],
+};
--- backend/src/routes/users-languages/users-languages.service.js
+++ backend/src/routes/users-languages/users-languages.service.js
@@ -3,40 +3,49 @@ const knex = require("../../database/connection");
 const table = "users-languages";
 
 function list() {
-    return knex(table)
-        .select("*");
+	return knex(table).select("*");
 }
 
 function find(userLanguageId) {
-    return knex(table)
-        .select("*")
-        .where({
-            id: userLanguageId
-        });
+	return knex(table).select("*").where({
+		id: userLanguageId,
+	});
 }
 
 function findByUser(userId) {
-    return knex(table)
-        .select("*")
-        .where({
-            user: userId
-        });
+	return knex(table)
+		.join("languages", "languages.id", "=", `${table}.language`)
+		.select(
+			"languages.name",
+			`${table}.language`,
+			`${table}.user`,
+			`${table}.id as user-language`
+		)
+		.where({
+			user: userId,
+		});
 }
+
+function findOneByUserandLanguage(data) {
+	return knex(table)
+		.select("*")
+		.where("user", data.user)
+		.where("language", data.language);
+}
+
 function create(data) {
-    return knex(table)
-        .insert(data, "*");
+	return knex(table).insert(data, "*");
 }
 
 function remove(userLanguageId) {
-    return knex(table)
-        .where({ id: userLanguageId })
-        .del();
+	return knex(table).where({ id: userLanguageId }).del();
 }
 
 module.exports = {
-    list,
-    find,
-    findByUser,
-    create,
-    remove,
-}
+	list,
+	find,
+	findByUser,
+    findOneByUserandLanguage,
+	create,
+	remove,
+};
