dbcdd22cc266f43bbed600aab39cb32b339411c6
a-w-m <mamrey@gmail.com>
2021-08-23 13:52:07 -0400

    Update edit-profile frontend requests

--- frontend/src/services/edit-profile/index.js
+++ frontend/src/services/edit-profile/index.js
@@ -7,7 +7,7 @@ export async function postSkill(name) {
     let response = await fetch(`${config.API_ENDPOINT}/skills/`, {
       method: "POST",
       body: JSON.stringify({
-        data: {name},
+        data: {name: name.toLowerCase()},
       }),
       headers: {
         "Content-Type": "application/json",
@@ -26,9 +26,54 @@ export async function postSkill(name) {
   }
 
 
-  export async function deleteUserSkill(id) {
+  export async function deleteUserSkill(userSkill) {
 
-    let response = await fetch(`${config.API_ENDPOINT}/users-skills/${id}`, {
+    let response = await fetch(`${config.API_ENDPOINT}/users-skills/${userSkill}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAuthToken()}`,
+
+      },
+    });
+  
+    const json = await response.json();
+  
+    if (!response.ok) {
+      return Promise.reject({ message: json.error });
+    }
+  
+    return json;
+  }
+
+
+  export async function postLanguage(name) {
+
+    let response = await fetch(`${config.API_ENDPOINT}/languages/`, {
+      method: "POST",
+      body: JSON.stringify({
+        data: {name: name.toLowerCase()},
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAuthToken()}`,
+
+      },
+    });
+  
+    const json = await response.json();
+  
+    if (!response.ok) {
+      return Promise.reject({ message: json.error });
+    }
+  
+    return json;
+  }
+
+
+  export async function deleteUserLanguage(userLanguage) {
+
+    let response = await fetch(`${config.API_ENDPOINT}/users-languages/${userLanguage}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -48,9 +93,13 @@ export async function postSkill(name) {
 
 
 
+
 const editProfile ={
     postSkill,
-    deleteUserSkill
+    deleteUserSkill,
+    postLanguage,
+    deleteUserLanguage,
+
 }
 
 export default editProfile
