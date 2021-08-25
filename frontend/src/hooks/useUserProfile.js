4e41c2d596473185072e0525a79c73670571ecf4
a-w-m <mamrey@gmail.com>
2021-08-25 18:47:42 -0400

    Refactor Profile - make profile component reusable

--- frontend/src/hooks/useUserProfile.js
+++ frontend/src/hooks/useUserProfile.js
@@ -1,41 +1,36 @@
-import {useState, useEffect} from 'react'
-import profileServices from '../services/profile'
+import { useState, useEffect } from "react";
+import profileServices from "../services/profile";
 
-function useUserProfile(userId, dispatch){
-    const [error, setError] = useState()
-    const [loading, setLoading] = useState()
+function useUserProfile(userId, dispatch) {
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 
-    if(!userId){ return }
+		async function fetchData() {
 
-        async function fetchData(){
+            setError(false)
+			setLoading(true);
 
-            setLoading(true)
-        try{
-            const user = await profileServices.getUser(userId)
-            const skills = await profileServices.getUserSkills(userId)
-            const languages = await profileServices.getUserLanguages(userId)
-            dispatch({ type: "SET_PROFILE_SKILLS", payload:skills.data });
-            dispatch({ type: "SET_PROFILE_GOAL", payload: user.data.goal });
-            dispatch({ type: "SET_PROFILE_LANGUAGES", payload: languages.data });
+			try {
+				const user = await profileServices.getUser(userId);
+				const skills = await profileServices.getUserSkills(userId);
+				const languages = await profileServices.getUserLanguages(userId);
+				dispatch({ type: "SET_PROFILE_SKILLS", payload: skills.data });
+				dispatch({ type: "SET_PROFILE_GOAL", payload: user.data.goal });
+				dispatch({ type: "SET_PROFILE_NAME", payload: user.data.name });
+				dispatch({ type: "SET_PROFILE_LANGUAGES", payload: languages.data });
+			} catch (error) {
+				setError(true);
+            }
 
+			setLoading(false);
+		}
 
-        } 
-        catch (error){
-            setError(true)
-        }
+		fetchData();
+	}, [userId, dispatch]);
 
-        setLoading(false)
-
-    }
-
-        fetchData()
-
-    }, [userId, dispatch]);
-    
-    return [error, loading]
-
+	return [error, loading];
 }
 
-export default useUserProfile
+export default useUserProfile;
