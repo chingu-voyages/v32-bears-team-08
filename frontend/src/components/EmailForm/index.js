0561d4afddb3da9945a55c8e8b34f6889ae39cbe
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-08-31 13:32:31 -0700

    added state logic in EmailForm component

--- frontend/src/components/EmailForm/index.js
+++ frontend/src/components/EmailForm/index.js
@@ -1,8 +1,47 @@
+import React, { useReducer } from "react";
 import styles from "./style.module.css";
 
+const initialState = {
+  subject: "",
+  message: "",
+  sent: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_SUBJECT":
+      return {
+        ...state,
+        subject: action.payload,
+      };
+    case "SET_MESSAGE":
+      return {
+        ...state,
+        message: action.payload,
+      };
+    case "SET_SENT":
+      return {
+        ...state,
+        sent: true,
+      };
+    default:
+      return { ...state };
+  }
+}
+
 function EmailForm() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   async function handleSubmit(e) {
     e.preventDefault();
+
+    const data = {
+      subject: state.subject,
+      message: state.message,
+    };
+    console.log(data);
+
+    dispatch({ type: "SET_SENT", payload: false });
   }
 
   return (
@@ -17,22 +56,31 @@ function EmailForm() {
           Subject
         </label>
         <input
-          type="text"
-          id="subject"
-          name="subject"
-          placeholder="Subject"
+          type={"text"}
+          id={"subject"}
+          name={"subject"}
+          placeholder={"Subject"}
+          value={state.subject}
           className={styles["email__form__input"]}
+          onChange={(e) => {
+            dispatch({ type: "SET_SUBJECT", payload: e.target.value });
+          }}
         ></input>
+
         <label htmlFor="message" className={styles["email__form__label"]}>
           Message
         </label>
         <textarea
-          type="text"
-          id="message"
-          name="message"
-          rows="10"
-          placeholder="Message"
+          type={"text"}
+          id={"message"}
+          name={"message"}
+          rows={"10"}
+          placeholder={"Message"}
+          value={state.message}
           className={styles["email__form__textarea"]}
+          onChange={(e) => {
+            dispatch({ type: "SET_MESSAGE", payload: e.target.value });
+          }}
         />
 
         <button className={styles["email__form__button"]} type="submit">
