f2a4371d936f9ecdea0c56b45f37e255b6f7f7cd
a-w-m <mamrey@gmail.com>
2021-09-01 13:57:10 -0400

    Add title to dashboard and responsive styling

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -25,6 +25,9 @@ function Dashboard(props) {
 
   return (
     <div className={`container ${styles["dashboard"]}`}>
+      <div className = {styles["title-container"]}>
+        <h2 className = {styles["title"]}>Recommended learning partners, curated just for you.</h2>
+      </div>
       <div className={styles["dashboard-left"]}>
         {recommendedUsers.map((user) => (
           <Userinfo {...user} />
