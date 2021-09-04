14d88d0c8be6c088a46baec76ec3e9edfc9568c0
a-w-m <mamrey@gmail.com>
2021-09-03 20:51:12 -0400

    serve create-react-app build files

--- backend/src/app.js
+++ backend/src/app.js
@@ -45,4 +45,14 @@ app.use("/users-resources", usersResourcesRouter)
 app.use(notFound);
 app.use(errorHandler);
 
+
+//Serve Static Assets in production
+//set static folder
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+  }
+
 module.exports = app;
